32913b34b60059d09494f27ab0c0d582b6073ad4
Andrey <duzhak.a55@gmail.com>
2021-04-22 11:52:40 +0300

    course_work

---
+++ BDinterface/BDinterface/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace BDinterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.прізвищеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.імяDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.імяПоБатьковіDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.преподавателиBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.db_LecturersDataSet = new BDinterface.Db_LecturersDataSet();
+            this.преподавателиTableAdapter = new BDinterface.Db_LecturersDataSetTableAdapters.ПреподавателиTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ChangeRank = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.преподавателиBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_LecturersDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn,
+            this.прізвищеDataGridViewTextBoxColumn,
+            this.імяDataGridViewTextBoxColumn,
+            this.імяПоБатьковіDataGridViewTextBoxColumn,
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.преподавателиBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(859, 402);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // кодDataGridViewTextBoxColumn
+            // 
+            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
+            this.кодDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // прізвищеDataGridViewTextBoxColumn
+            // 
+            this.прізвищеDataGridViewTextBoxColumn.DataPropertyName = "Прізвище";
+            this.прізвищеDataGridViewTextBoxColumn.HeaderText = "Прізвище";
+            this.прізвищеDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.прізвищеDataGridViewTextBoxColumn.Name = "прізвищеDataGridViewTextBoxColumn";
+            this.прізвищеDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // імяDataGridViewTextBoxColumn
+            // 
+            this.імяDataGridViewTextBoxColumn.DataPropertyName = "Ім\'я";
+            this.імяDataGridViewTextBoxColumn.HeaderText = "Ім\'я";
+            this.імяDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.імяDataGridViewTextBoxColumn.Name = "імяDataGridViewTextBoxColumn";
+            this.імяDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // імяПоБатьковіDataGridViewTextBoxColumn
+            // 
+            this.імяПоБатьковіDataGridViewTextBoxColumn.DataPropertyName = "Ім\'я по батькові";
+            this.імяПоБатьковіDataGridViewTextBoxColumn.HeaderText = "Ім\'я по батькові";
+            this.імяПоБатьковіDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.імяПоБатьковіDataGridViewTextBoxColumn.Name = "імяПоБатьковіDataGridViewTextBoxColumn";
+            this.імяПоБатьковіDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // вченеЗванняВикладачаDataGridViewTextBoxColumn
+            // 
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn.DataPropertyName = "Вчене звання викладача";
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn.HeaderText = "Вчене звання викладача";
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn.Name = "вченеЗванняВикладачаDataGridViewTextBoxColumn";
+            this.вченеЗванняВикладачаDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // преподавателиBindingSource
+            // 
+            this.преподавателиBindingSource.DataMember = "Преподаватели";
+            this.преподавателиBindingSource.DataSource = this.db_LecturersDataSet;
+            // 
+            // db_LecturersDataSet
+            // 
+            this.db_LecturersDataSet.DataSetName = "Db_LecturersDataSet";
+            this.db_LecturersDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // преподавателиTableAdapter
+            // 
+            this.преподавателиTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 429);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вкажіть код викладача";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 429);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Вкажіть код викладача";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(636, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Вкажіть нове звання викладача";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 462);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 462);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(639, 462);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(419, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 76);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Видалити викладача";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ChangeRank
+            // 
+            this.ChangeRank.Location = new System.Drawing.Point(376, 508);
+            this.ChangeRank.Name = "ChangeRank";
+            this.ChangeRank.Size = new System.Drawing.Size(682, 76);
+            this.ChangeRank.TabIndex = 8;
+            this.ChangeRank.Text = "Змінити вчене звання";
+            this.ChangeRank.UseVisualStyleBackColor = true;
+            this.ChangeRank.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(877, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 402);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Додати викладача";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1065, 598);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ChangeRank);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Кафедра";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.преподавателиBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_LecturersDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Db_LecturersDataSet db_LecturersDataSet;
+        private System.Windows.Forms.BindingSource преподавателиBindingSource;
+        private Db_LecturersDataSetTableAdapters.ПреподавателиTableAdapter преподавателиTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn прізвищеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn імяDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn імяПоБатьковіDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn вченеЗванняВикладачаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ChangeRank;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BDinterface/BDinterface/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace BDinterface
+{
+    
+    public partial class Form1 : Form
+    {
+        public static string connectString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = D:\\0 Cursach\\BDinterface\\BDinterface\\Db Lecturers.mdb";
+        private OleDbConnection myConnection;
+        public Form1()
+        {
+           
+                InitializeComponent();
+                myConnection = new OleDbConnection(connectString);
+                myConnection.Open();
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "db_LecturersDataSet.Преподаватели". При необходимости она может быть перемещена или удалена.
+            this.преподавателиTableAdapter.Fill(this.db_LecturersDataSet.Преподаватели);
+
+        }
+
+        
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            int code = Convert.ToInt32(textBox1.Text);
+            string query = "DELETE FROM Преподаватели WHERE [Код] =" + code;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Дані про викладача видалені");
+            this.преподавателиTableAdapter.Fill(this.db_LecturersDataSet.Преподаватели);
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+           
+            int code = Convert.ToInt32(textBox2.Text);
+            string query = "UPDATE Преподаватели SET Вчене звання викладача = '" + textBox3.Text + "' WHERE [Код] = " + code;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Дані про викладача змінено");
+            this.преподавателиTableAdapter.Fill(this.db_LecturersDataSet.Преподаватели);
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConnection.Close();
+
+        }
+
+
+    }
+}
